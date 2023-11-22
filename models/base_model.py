651bbeef5e314062a4596ac27dc1768ea8a936a6
root <carlosfrancoart@gmail.com>
2023-11-22 04:13:10 +0000

    task6 base

--- models/base_model.py
+++ models/base_model.py
@@ -2,18 +2,32 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, String, datetime
+from sqlalchemy.ext.declarative import declaration_base
 
+
+Base = declaration_base()
 
 class BaseModel:
+    """id for Base"""
+    id = Column(String(60), primary_key=True, nullable=False, 
+    default= str(uuid.uuid4()))
+    created_at = Column(datetime(), nullable=False, default=datetime.utcnow())
+    updated_at = Column(datetime(), nullable=False, default=datetime.utcnow())
+    
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
+        for key, value in kwargs.items():
+            if not hasattr(self, key):
+                setattr(self, key, value)
+        
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,7 +45,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -42,3 +56,6 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+    def delete(self):
+        from models import storage
+        storage.delete(self)
