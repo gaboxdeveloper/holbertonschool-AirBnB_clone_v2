2f26dfad7d9c4b019139a454b2552f30bb6b965b
root <carlosfrancoart@gmail.com>
2023-11-23 01:41:41 +0000

    engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -19,4 +19,47 @@ class DBStorage():
         "mysql+mysqldb://{}:{}@{}/{}".format(usr, passwd, host, db),
         pool_pre_ping=True)
     
-    
+    if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """shows list of all classes"""
+        objects = {}
+        if cls is None:
+            classes = ['State', 'City', 'User', 'Place', 'Review', 'Amenity']
+            objs = []
+            for _class in classes:
+                query = self.__session.query(eval(_class))
+                for res in query:
+                    objs.append(res)
+        else:
+            objs = self.__session.query(cls).all()
+        for value in objs:
+            key = type(value).__name__+'.'+str(value.id)
+            objects[key] = value
+        return objects
+
+    def new(self, obj):
+        """Adds a new object to the current session"""
+        if obj:
+            self.__session.add(obj)
+
+    def save(self):
+        """Saves the current session to the database"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Deletes the object from the current session if not None"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Creates all tables in the database and starts the session"""
+        Base.metadata.create_all(self.__engine)
+        ses = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        session = scoped_session(ses)
+        self.__session = session
+
+    def close(self):
+        """Closes the current session"""
+        self.__session.remove()
