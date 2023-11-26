b8ffa6bd9d8040ade1cf2fd6bb12110f8be8d514
root <carlosfrancoart@gmailcom>
2023-11-26 04:25:53 +0000

    task9 v3

--- models/place.py
+++ models/place.py
@@ -17,8 +17,8 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
 
-    city_id =Column(String(60), ForeignKey('cities.id') nullable=False) 
-    user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
+    city_id =Column(String(60), ForeignKey('cities.id'), nullable=False) 
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=False)
     number_rooms = Column(Interger, default=0, nullable=False)
