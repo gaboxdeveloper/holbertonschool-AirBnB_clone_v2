7f4cbed4c2d7b1c8fac3e8225151a271533487ae
root <carlosfrancoart@gmailcom>
2023-11-26 04:18:18 +0000

    task9 v3

--- models/state.py
+++ models/state.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
+import models
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
-import models
+
 
 
 class State(BaseModel, Base):
