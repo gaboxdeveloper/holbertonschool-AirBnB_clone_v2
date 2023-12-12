4907c29d341988ed14a96c9035b374f682fa930f
Gabriel <grossy.sjb@gmail.com>
2023-12-12 13:42:05 +0000

    task1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""documentation importing Flask"""
+from flask import Flask
+"""documentation calling flask"""
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """function to return Hello HBNB"""
+    return 'Hello HBNB!'
+
+@app.route('/HBNB', strict_slashes=False)
+def hello_hbnb():
+    """function to return HBNB"""
+    return 'HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
