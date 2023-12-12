93f0367fa6f1afc2c81e1fa8221b202e7579e5e1
Gabriel <grossy.sjb@gmail.com>
2023-12-12 14:59:24 +0000

    task3 v3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -5,38 +5,33 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """function to return Hello HBNB"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     """function to return HBNB"""
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def c_isfun(text):
     """function to return C"""
     formatted_text = text.replace('_', ' ')
     return f'C {formatted_text}'
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is_cool'})
+@app.route('/python/<text>',)
 def python(text="is cool"):
     """function to return python"""
     formatted_text = text.replace('_', ' ')
     return f'Python {formatted_text}'
 
 
-@app.route('/python/<text>', strict_slashes=False)
-def python_(text):
-    """function to return python"""
-    formatted_text = text.replace('_', ' ')
-    return f'Python {formatted_text}'
-
-
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
