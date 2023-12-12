623db500330404676c45cc7c1ca63adff19ebd7b
Gabriel <grossy.sjb@gmail.com>
2023-12-12 14:18:16 +0000

    task2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -17,5 +17,12 @@ def hbnb():
     return 'HBNB'
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def c_isfun(text):
+    """function to return C"""
+    formatted_text = text.replace('_', ' ')
+    return f'C{formatted_text}'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
