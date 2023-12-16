aa5b3b2f98de7695ce30464e57300a503db1b20e
Gabriel <grossy.sjb@gmail.com>
2023-12-16 19:48:25 +0000

    task4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""documentation importing Flask"""
+from flask import Flask
+"""documentation calling flask"""
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """function to return Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """function to return HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_isfun(text):
+    """function to return C"""
+    formatted_text = text.replace('_', ' ')
+    return f'C {formatted_text}'
+
+
+@app.route('/python/', defaults={'text': 'is_cool'})
+@app.route('/python/<text>',)
+def python(text="is cool"):
+    """function to return python"""
+    formatted_text = text.replace('_', ' ')
+    return f'Python {formatted_text}'
+
+
+@app.route('/number/<n>')
+def number(n):
+    """function to return a number"""
+    if isinstance(n, int):
+        return n
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
